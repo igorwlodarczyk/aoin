e230d77d2744c2e48c5e19d078f7658f4b05c2ba
Igor Wlodarczyk <wlodar.igor@gmail.com>
2025-12-27 21:37:10 +0100

    added dynamic name to output log

--- src/northpole_packing/experiments.py
+++ src/northpole_packing/experiments.py
@@ -1,9 +1,10 @@
 import sys
+import uuid
 from northpole_packing.genetic_algorithm import GeneticAlgorithm
 
 
 ga = GeneticAlgorithm(
-    "output.log",
+    f"output_{uuid.uuid4().hex}.log",
     num_trees=100,
     num_generations=sys.maxsize
 )
